90009b5aa1b697f65a294097a8e77882d8f744e8
PratikshaPatare17 <pritu178@gmail.com>
2019-06-04 17:20:03 +0530

    star2

--- Abc/src/ab/star1.java
+++ Abc/src/ab/star1.java
@@ -3,7 +3,8 @@ package ab;
 public class star1 {
 
 	public static void main(String[] args) {
-System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("New changes......");
 	}
 
 }
---
+++ Abc/src/ab/star2.java
@@ -0,0 +1,10 @@
+package ab;
+
+public class star2 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Star 2............");
+	}
+
+}
