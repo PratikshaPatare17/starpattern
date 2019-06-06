1a6480acdd6f638eb661c940b95e637dea158bd5
PratikshaPatare17 <pritu178@gmail.com>
2019-06-06 13:48:01 +0530

    new stars

--- Abc/src/ab/Star3.java
+++ Abc/src/ab/Star3.java
@@ -1,23 +1,17 @@
 package ab;
 
 /*
- 		*
- 	 *  *  *
-  *  *  *  *  *
-     *  *  *
- 	    *   
+ 	*  *  *  *  *
+ 	   *  *  *
+ 	      *   
  */
 
-public class star1 {
-
+public class Star3 {
+	
 	public static void main(String[] args) {
 		
-		int mid=3;
-		
-		for(int i=1;i<=3;i++)
+		for(int i=3;i>=1;i--)
 		{
-			if(i<=mid)
-			{
 			for(int k=1;k<=3-i;k++)
 			{
 				System.out.print(" ");
@@ -25,11 +19,6 @@ public class star1 {
 			for(int j=1;j<=2*i-1;j++)
 			{
 				System.out.print("*");
-			}
-			}
-			else
-			{
-				
 			}
 			System.out.println();
 		}
--- Abc/src/ab/star2.java
+++ Abc/src/ab/star2.java
@@ -1,10 +1,28 @@
 package ab;
 
+/*
+ 		*
+ 	 *  *  *
+  *  *  *  *  *
+  
+ */
+
 public class star2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Star 2............");
+		for(int i=1;i<=3;i++)
+		{
+			for(int k=1;k<=3-i;k++)
+			{
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
